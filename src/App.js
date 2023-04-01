532f985407bebee9ea8df5826b0b9ca1084f696c
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-04-01 11:17:16 +0300

    first

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
+
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
           <Route path='/:user' component={Card} />
